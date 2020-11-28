dee1ff357467b591b2c13214f406ce41d2bd5f20
felipexy <felipexy@hotmail.com>
2020-11-28 05:11:05 -0300

    range slider changing colors -- needs to fiz the white dots

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,22 +4,37 @@ import { useDispatch } from 'react-redux';
 
 const Slider = ({min, max}) => {
 
-    const [rangeVal, setRangeVal] = useState(2);
+    let [rangeVal, setRangeVal] = useState(2);
     const dispatch = useDispatch();
 
     const handleChange = (event) => {
         setRangeVal(event.target.value)
+        console.log(event.target.value);
+        document.getElementById("Slider").style.background = `linear-gradient(90deg, rgb(139,185,38) ${event.target.value*25}%, rgb(118,0,117) ${event.target.value*25}%)`       
         dispatch({ type: 'CHANGE_ID', id: event.target.value } )
     }
 
+
+
     return (
-        <div className="sliderContainer"> 
-            <input type="range" className="slider" min={min} max={max} value={rangeVal}
-            onChange={(event) => handleChange(event)}/>
+        <div>
+            <div className="sliderContainer"> 
+                <input id="Slider" type="range" className="slider" min={min} max={max} value={rangeVal} step="1"
+                onChange={(event) => handleChange(event)}/>
+            </div>
+            <div>
+                <ul className="range-labels">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>    
         </div>
     );
   };
-  
+
   export default Slider;
 
 
