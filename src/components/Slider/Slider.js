027d80bbc9a0774774235be886d0a72ab9354039
felipexy <felipexy@hotmail.com>
2020-11-28 19:58:01 -0300

    Responsive slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -4,35 +4,40 @@ import { useDispatch } from 'react-redux';
 
 const Slider = ({min, max}) => {
 
-    let [rangeVal, setRangeVal] = useState(2);
-    let [lastValue, setlastValue] = useState(2);
+    let [rangeVal, setRangeVal] = useState(Math.floor(max/2));
+    let [lastValue, setlastValue] = useState(Math.floor(max/2));
     const dispatch = useDispatch();
 
-
-    console.log(lastValue);
-
-
-    const handleChange = (event) => {
+    const handleChange = (event, max) => {
         document.getElementById(`t${lastValue}`).style.zIndex = "1"; 
         setlastValue(event.target.value);
         setRangeVal(event.target.value)
-        document.getElementById("Slider").style.background = `linear-gradient(90deg, rgb(139,185,38) ${event.target.value*25}%, rgb(118,0,117) ${event.target.value*25}%)` 
+        document.getElementById("Slider").style.background = `linear-gradient(90deg, rgb(139,185,38) ${event.target.value*((1/max)*100)}%, rgb(118,0,117) ${event.target.value*((1/max)*100)}%)` 
         document.getElementById(`t${event.target.value}`).style.zIndex = "-1";   
         dispatch({ type: 'CHANGE_ID', id: event.target.value } )
+    }
+
+    var lista = [];
+    function listOfDots(){ 
+        for (var i = 0; i <= max; i++){
+             
+            if(i === Math.floor(max/2)){
+                lista.push(<li key={i} id={`t${i}`} className="tickerInvisible"></li>)
+            } else{
+            lista.push(<li key={i} id={`t${i}`} className="ticker"></li>)
+            }
+        }
     }
 
     return (
         <div>
             <div className="sliderContainer"> 
                 <input id="Slider" type="range" className="slider" min={min} max={max} value={rangeVal} step="1"
-                onChange={(event) => handleChange(event)}/>
+                onChange={(event) => handleChange(event,max)} style={{background: `linear-gradient(90deg, rgb(139,185,38) ${rangeVal*((1/max)*100)}%, rgb(118,0,117) ${rangeVal*((1/max)*100)}%)`}}/>
             </div>
                 <ul className="range-tickers">
-                    <li id="t0" className="ticker"></li>
-                    <li id="t1" className="ticker"></li>
-                    <li id="t2" className="tickerInvisible"></li>
-                    <li id="t3" className="ticker"></li>
-                    <li id="t4" className="ticker"></li>
+                    {listOfDots()}
+                    {lista}
                 </ul>
         </div>
     );
